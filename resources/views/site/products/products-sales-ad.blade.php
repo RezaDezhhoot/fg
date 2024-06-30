05dcd3c65ffb447bc9eabc074aa2fa6ea8a2b9f2
sadegh taherifar <110782912+sadeghtaheryfar@users.noreply.github.com>
2024-06-30 18:41:17 +0330

    chebge file

--- resources/views/site/products/products-sales-ad.blade.php
+++ resources/views/site/products/products-sales-ad.blade.php
@@ -11,10 +11,10 @@
         </div>
 
         <div class="bg-white w-full p-4" style="border-radius: 1rem">
-            <div id="box-header-box-hever-shop w-full">
+            <div id="box-header-box-hever-shop">
                 <ul>
                     <a wire:click="$set('category','')">
-                        <li class="item-box-hever-store header-box-hever-shop cursor-pointer">
+                        <li class="item-box-hever-store header-box-hever-shop cursor-pointer active">
                             <div class="img">
                                 <img src="https://farsgamer.com/media/6635e71a0ac06.png" alt="">
                             </div>
@@ -22,9 +22,10 @@
                             <span>همه دسته بندی ها</span>
                         </li>
                     </a>
-                    @foreach($categories as $item)
-                        <a wire:click="$set('category','{{$item->id}}')" >
-                            <li class="item-box-hever-store header-box-hever-shop {{ $item->id == $category ? "header-box-hever-shop-activate" : "" }} cursor-pointer">
+                    @foreach ($categories as $item)
+                        <a wire:click="$set('category','{{ $item->id }}')">
+                            <li
+                                class="item-box-hever-store active header-box-hever-shop {{ $item->id == $category ? 'header-box-hever-shop-activate' : '' }} cursor-pointer">
                                 <div class="img">
                                     <img src="{{ asset($item->icon) }}" alt="{{ $item->title }}">
                                 </div>
@@ -69,10 +70,10 @@
                     </div>
 
                     <div class="range-slider" wire:ignore>
-                        <input type="range" id="rangeMin1" min="0" max="{{ $max }}" value="{{ $min }}"
-                            step="10" wire:model="min">
-                        <input type="range" id="rangeMax1" min="0" max="{{ $max }}" value="{{ $max }}"
-                            step="10" wire:model="max">
+                        <input type="range" id="rangeMin1" min="0" max="{{ $max }}"
+                            value="{{ $min }}" step="10" wire:model="min">
+                        <input type="range" id="rangeMax1" min="0" max="{{ $max }}"
+                            value="{{ $max }}" step="10" wire:model="max">
                         <div class="track"></div>
                         <div class="range-track" id="rangeTrack1"></div>
                     </div>
@@ -114,8 +115,7 @@
     {{-- این قسمت فیلتر ها در ریسپانسیو است ....................................................... --}}
     <section id="box-search-mobile">
         <div class="right-search-mobile width-50" data-bs-toggle="modal" data-bs-target="#modal-filters">
-            <svg width="14" height="14" viewBox="0 0 14 14" fill="none"
-                xmlns="http://www.w3.org/2000/svg">
+            <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path
                     d="M3.15104 1.2251H10.851C11.4927 1.2251 12.0177 1.7501 12.0177 2.39176V3.6751C12.0177 4.14176 11.726 4.7251 11.4344 5.01676L8.92604 7.23343C8.57604 7.5251 8.34271 8.10843 8.34271 8.5751V11.0834C8.34271 11.4334 8.10937 11.9001 7.81771 12.0751L7.00104 12.6001C6.24271 13.0668 5.19271 12.5418 5.19271 11.6084V8.51676C5.19271 8.10843 4.95938 7.58343 4.72604 7.29176L2.50938 4.95843C2.21771 4.66676 1.98438 4.14176 1.98438 3.79176V2.4501C1.98438 1.7501 2.50937 1.2251 3.15104 1.2251Z"
                     stroke="#808191" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round"
@@ -161,40 +161,44 @@
                     <ul class="box-filter-search-mo">
                         <li wire:click="$set('sort','views')" class="item-filter-search-mo">
 
-                            @if($sort == 'views')
-                                <svg class="img-item-filter-search-mo" width="12" height="9" viewBox="0 0 12 9"
-                                     fill="none" xmlns="http://www.w3.org/2000/svg">
+                            @if ($sort == 'views')
+                                <svg class="img-item-filter-search-mo" width="12" height="9"
+                                    viewBox="0 0 12 9" fill="none" xmlns="http://www.w3.org/2000/svg">
                                     <path d="M10.5 1L4 7.5L1 5" stroke="#3D42DF" stroke-width="2"
-                                          stroke-linecap="round" />
+                                        stroke-linecap="round" />
                                 </svg>
                             @endif
 
 
-                            <span class="txt-item-filter-search-mo {{ $sort == 'views' ? 'color-blue' : '' }}">پربازدید ترین</span>
+                            <span
+                                class="txt-item-filter-search-mo {{ $sort == 'views' ? 'color-blue' : '' }}">پربازدید
+                                ترین</span>
                         </li>
 
                         <li wire:click="$set('sort','latest')" class="item-filter-search-mo">
 
-                            @if($sort == 'latest')
-                                <svg class="img-item-filter-search-mo" width="12" height="9" viewBox="0 0 12 9"
-                                     fill="none" xmlns="http://www.w3.org/2000/svg">
+                            @if ($sort == 'latest')
+                                <svg class="img-item-filter-search-mo" width="12" height="9"
+                                    viewBox="0 0 12 9" fill="none" xmlns="http://www.w3.org/2000/svg">
                                     <path d="M10.5 1L4 7.5L1 5" stroke="#3D42DF" stroke-width="2"
-                                          stroke-linecap="round" />
+                                        stroke-linecap="round" />
                                 </svg>
                             @endif
 
-                            <span class="txt-item-filter-search-mo {{ $sort == 'latest' ? 'color-blue' : '' }}">جدید ترین</span>
+                            <span class="txt-item-filter-search-mo {{ $sort == 'latest' ? 'color-blue' : '' }}">جدید
+                                ترین</span>
                         </li>
 
                         <li wire:click="$set('sort','amount')" class="item-filter-search-mo">
-                            @if($sort == 'amount')
-                                <svg class="img-item-filter-search-mo" width="12" height="9" viewBox="0 0 12 9"
-                                     fill="none" xmlns="http://www.w3.org/2000/svg">
+                            @if ($sort == 'amount')
+                                <svg class="img-item-filter-search-mo" width="12" height="9"
+                                    viewBox="0 0 12 9" fill="none" xmlns="http://www.w3.org/2000/svg">
                                     <path d="M10.5 1L4 7.5L1 5" stroke="#3D42DF" stroke-width="2"
-                                          stroke-linecap="round" />
+                                        stroke-linecap="round" />
                                 </svg>
                             @endif
-                            <span class="txt-item-filter-search-mo {{ $sort == 'amount' ? 'color-blue' : '' }}">گران ترین </span>
+                            <span class="txt-item-filter-search-mo {{ $sort == 'amount' ? 'color-blue' : '' }}">گران
+                                ترین </span>
                         </li>
                     </ul>
                 </div>
@@ -218,7 +222,7 @@
 
                     <span class="modal-title" id="exampleModalLabel">فیلتر ها</span>
 
-{{--                    <button class="btn-delete-filter-search-mo">حذف فیلتر</button>--}}
+                    {{--                    <button class="btn-delete-filter-search-mo">حذف فیلتر</button> --}}
                 </div>
 
                 <div class="modal-body text-center modal-filters">
@@ -240,7 +244,8 @@
                             <span class="txt-item-filter-search-mo">کالاهای موجود</span>
 
                             <div class="checkbox-container">
-                                <input type="checkbox" id="checkbox2" value="1" class="checkbox" wire:model="fg_depot">
+                                <input type="checkbox" id="checkbox2" value="1" class="checkbox"
+                                    wire:model="fg_depot">
 
                                 <label for="checkbox2" class="checkbox-label">
                                     <span class="checkbox-icon unchecked">
@@ -283,7 +288,7 @@
 
                     <span class="modal-title" id="exampleModalLabel">محدوده قیمت</span>
 
-{{--                    <button class="btn-delete-filter-search-mo">حذف فیلتر</button>--}}
+                    {{--                    <button class="btn-delete-filter-search-mo">حذف فیلتر</button> --}}
                 </div>
 
                 <div class="modal-body modal-filters">
@@ -291,7 +296,7 @@
                         <div class="box-price-input">
                             <div class="price-input">
                                 <span class="txt2-box-price">تا</span>
-                                <h2 class="input-min massage-box-price" id="rangeMaxValue2">{{$max }}</h2>
+                                <h2 class="input-min massage-box-price" id="rangeMaxValue2">{{ $max }}</h2>
                                 <span class="txt1-box-price">تومان</span>
                             </div>
 
@@ -303,10 +308,10 @@
                         </div>
 
                         <div class="range-slider" wire:ignore>
-                            <input type="range" id="rangeMin2"  max="{{$max }}" value="{{ $min }}"
-                                step="10" wire:model="min">
-                            <input type="range" id="rangeMax2"  max="{{$max }}" value="{{$max }}"
-                                step="10" wire:model="max">
+                            <input type="range" id="rangeMin2" max="{{ $max }}"
+                                value="{{ $min }}" step="10" wire:model="min">
+                            <input type="range" id="rangeMax2" max="{{ $max }}"
+                                value="{{ $max }}" step="10" wire:model="max">
                             <div class="track"></div>
                             <div class="range-track" id="rangeTrack2"></div>
                         </div>
@@ -333,86 +338,108 @@
 
             <div>
                 <ul class="flex-box">
-                    <li wire:click="$set('sort','views')" class="margin-horizontal-1 item-filter-header-search {{ $sort == 'views' ? 'color-blue' : '' }}">پربازدید ترین</li>
+                    <li wire:click="$set('sort','views')"
+                        class="margin-horizontal-1 item-filter-header-search {{ $sort == 'views' ? 'color-blue' : '' }}">
+                        پربازدید ترین</li>
 
-                    <li wire:click="$set('sort','latest')" class="margin-horizontal-1 item-filter-header-search {{ $sort == 'latest' ? 'color-blue' : '' }}">جدید ترین</li>
+                    <li wire:click="$set('sort','latest')"
+                        class="margin-horizontal-1 item-filter-header-search {{ $sort == 'latest' ? 'color-blue' : '' }}">
+                        جدید ترین</li>
 
-                    <li wire:click="$set('sort','amount')" class="margin-horizontal-1 item-filter-header-search {{ $sort == 'amount' ? 'color-blue' : '' }}">گران ترین</li>
+                    <li wire:click="$set('sort','amount')"
+                        class="margin-horizontal-1 item-filter-header-search {{ $sort == 'amount' ? 'color-blue' : '' }}">
+                        گران ترین</li>
                 </ul>
             </div>
         </div>
 
         <div class="hide-item-mobile">
-                <div class="bg-white w-full p-4 mb-4" {{empty($category) ? "" :'hidden'}}  style="border-radius: 1rem">
-                    <div class="fw-bold text-black mb-5">
-                        <h3 class="fs-4">دسته بندی</h3>
-                    </div>
-
-                    <div id="box-header-box-hever-shop-m">
-                        <a  wire:click="$set('category','')"  class="item-box-hever-store header-box-hever-shop d-flex flex-col">
-                            <div class="img">
-                                <img src="https://farsgamer.com/media/6635e71a0ac06.png" alt="">
-                            </div>
-
-                            <span>همه دسته بندی ها</span>
-                        </a>
-
-                        @foreach($categories as $item)
-                            <a wire:click="$set('category','{{$item->id}}')" class="item-box-hever-store header-box-hever-shop d-flex flex-col">
-                                <div class="img">
-                                    <img src="{{ asset($item->icon) }}" alt="{{ $item->title }}">
-                                </div>
-
-                                <span>{{ $item->title }}</span>
-                            </a>
-                        @endforeach
-                    </div>
+            <div class="bg-white w-full p-4 mb-4" {{ empty($category) ? '' : 'hidden' }} style="border-radius: 1rem">
+                <div class="fw-bold text-black mb-5">
+                    <h3 class="fs-4">دسته بندی</h3>
                 </div>
-                <div {{! empty($category) ? "" :'hidden'}} class="bg-white w-full p-4 mb-4  open-box-category  cursor-pointer" style="border-radius: 1rem">
-                    <div  class="fw-bold text-black flex-box flex-justify-space">
-                        <div class="flex-box">
-                            <svg class="icon-header-store-mbile icon-category-header-store ml-4 mr-0" width="24" height="24"
-                                 viewBox="0 0 16 18" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <path
-                                    d="M13.098 6.75012L8.9415 11.6401C8.45063 12.2176 7.64738 12.2176 7.1565 11.6401L3 6.75012"
-                                    stroke="black" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round"
-                                    stroke-linejoin="round" />
-                            </svg>
 
-
-                            <h3 class="fs-4 mr-2">دسته بندی</h3>
+                <div id="box-header-box-hever-shop-m">
+                    <a wire:click="$set('category','')"
+                        class="item-box-hever-store header-box-hever-shop d-flex flex-col">
+                        <div class="img">
+                            <img src="https://farsgamer.com/media/6635e71a0ac06.png" alt="">
                         </div>
 
-                        <div class="category-selected-sale">
-                            <img src="https://farsgamer.com/media/6635e71a0ac06.png" alt="محصولات فورتنایت">
+                        <span>همه دسته بندی ها</span>
+                    </a>
 
-                            <span>فورتنایت</span>
-                        </div>
-                    </div>
-
-                    <div class="message-category-store-mobile hide-item mt-8" id="box-header-box-hever-shop-m">
-                        <a  wire:click="$set('category','')"  class="item-box-hever-store header-box-hever-shop d-flex flex-col">
+                    @foreach ($categories as $item)
+                        <a wire:click="$set('category','{{ $item->id }}')"
+                            class="item-box-hever-store header-box-hever-shop d-flex flex-col">
                             <div class="img">
-                                <img src="https://farsgamer.com/media/6635e71a0ac06.png" alt="">
+                                <img src="{{ asset($item->icon) }}" alt="{{ $item->title }}">
                             </div>
 
-                            <span>همه دسته بندی ها</span>
+                            <span>{{ $item->title }}</span>
                         </a>
+                    @endforeach
+                </div>
+            </div>
+            <div {{ !empty($category) ? '' : 'hidden' }}
+                class="bg-white w-full p-4 mb-4  open-box-category  cursor-pointer" style="border-radius: 1rem">
+                <div class="fw-bold text-black flex-box flex-justify-space">
+                    <div class="flex-box">
+                        <svg class="icon-header-store-mbile icon-category-header-store ml-4 mr-0" width="24"
+                            height="24" viewBox="0 0 16 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path
+                                d="M13.098 6.75012L8.9415 11.6401C8.45063 12.2176 7.64738 12.2176 7.1565 11.6401L3 6.75012"
+                                stroke="black" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round"
+                                stroke-linejoin="round" />
+                        </svg>
 
-                        @foreach($categories as $item)
-                            <a wire:click="$set('category','{{$item->id}}')" class="item-box-hever-store {{ $item->id == $category ? "header-box-hever-shop-activate" : "" }} header-box-hever-shop d-flex flex-col">
-                                <div class="img">
-                                    <img src="{{ asset($item->icon) }}" alt="{{ $item->title }}">
-                                </div>
 
-                                <span>{{ $item->title }}</span>
-                            </a>
-                        @endforeach
+                        <h3 class="fs-4 mr-2">دسته بندی</h3>
+                    </div>
+
+                    <div class="category-selected-sale">
+                        <img src="https://farsgamer.com/media/6635e71a0ac06.png" alt="محصولات فورتنایت">
+
+                        <span>فورتنایت</span>
                     </div>
                 </div>
-            {{-- اگه دسته  بندی انتخاب کرده بود کاربر--}}
+
+                <div class="message-category-store-mobile hide-item mt-8" id="box-header-box-hever-shop-m">
+                    <a wire:click="$set('category','')"
+                        class="item-box-hever-store header-box-hever-shop d-flex flex-col">
+                        <div class="img">
+                            <img src="https://farsgamer.com/media/6635e71a0ac06.png" alt="">
+                        </div>
+
+                        <span>همه دسته بندی ها</span>
+                    </a>
+
+                    @foreach ($categories as $item)
+                        <a wire:click="$set('category','{{ $item->id }}')"
+                            class="item-box-hever-store {{ $item->id == $category ? 'header-box-hever-shop-activate' : '' }} header-box-hever-shop d-flex flex-col">
+                            <div class="img">
+                                <img src="{{ asset($item->icon) }}" alt="{{ $item->title }}">
+                            </div>
+
+                            <span>{{ $item->title }}</span>
+                        </a>
+                    @endforeach
+                </div>
+            </div>
+            {{-- اگه دسته  بندی انتخاب کرده بود کاربر --}}
 
 
+        </div>
+        {{-- اگه آگهی وجود نداشت ................ --}}
+
+        <div class="w-full h-full mt-10 pt-10 d-flex flex-column justify-content-center align-items-center">
+            <div>
+                <img src="https://farsgamer.com/media/667c3740edf65.png" alt="">
+            </div>
+
+            <div class="mt-4">
+                <p>متاسفانه هیچ آگهی ای یافت نشد</p>
+            </div>
         </div>
 
         <div class="hide-item-pc">
